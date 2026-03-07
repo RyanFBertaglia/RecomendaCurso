6b161fe3e6fe8069bc8af90324f2047664149f2b
Ryan Fernandes Bertaglia <ryanfernandesbertaglia@gmail.com>
2026-03-07 15:19:36 -0300

    test cep to coordinates compare two points

--- src/test/java/com/recommend/server/filter/FilterCoursesTest.java
+++ src/test/java/com/recommend/server/filter/FilterCoursesTest.java
@@ -1,4 +1,4 @@
-package com.recommend.server;
+package com.recommend.server.filter;
 
 import com.recommend.server.model.Course;
 import com.recommend.server.dto.CourseResponse;
@@ -10,8 +10,11 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 @SpringBootTest
-public class TestFilterCourses {
+public class FilterCoursesTest {
     @Test
     void testFilterCourses() {
         List<Course> courses = new ArrayList<>();
@@ -33,6 +36,12 @@ public class TestFilterCourses {
 
         System.out.println("Recommended courses:");
         possible.forEach(c -> System.out.println(c.course().getName() + " - Afinidade: " + c.incidence()));
+
+        assertEquals(1, possible.size(), "Should rest only one course");
+        assertEquals("Artes", possible.getFirst().course().getName());
+        assertEquals(2, possible.getFirst().incidence(), "O score de afinidade para Artes deveria ser 2");
+        boolean has = possible.stream().anyMatch(c -> c.course().getName().equals("Química"));
+        assertFalse(has, "Química doesn't match with the person");
     }
 
 }
--- src/test/java/com/recommend/server/location/CepToCoordinatesTest.java
+++ src/test/java/com/recommend/server/location/CepToCoordinatesTest.java
@@ -1,4 +1,65 @@
 package com.recommend.server.location;
 
-public class CepToCoordenatesTest {
+import com.recommend.server.dto.Coordinates;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class CepToCoordinatesTest {
+
+    private RestTemplate restTemplate;
+
+    @Test
+    void getALatLong() {
+        RestTemplate restTemplate = new RestTemplate();
+        int cep=13060473;
+
+        String json = restTemplate.getForObject(
+                "https://brasilapi.com.br/api/cep/v2/" + cep,
+                String.class
+        );
+
+        if (json == null) throw new AssertionError();
+        String latitude = json.split("\"latitude\":\"")[1].split("\"")[0];
+        String longitude = json.split("\"longitude\":\"")[1].split("\"")[0];
+
+        Coordinates coordinates = new Coordinates(Double.valueOf(latitude), Double.valueOf(longitude));
+        assertNotNull(coordinates, "The coordinates must exist");
+        IO.println(coordinates);
+    }
+
+    @Test
+    void getApproximated() {
+        // As a difference of 1/2 streets doesn't make any difference
+        // change a little the cep is not so bad
+        RestTemplate restTemplate = new RestTemplate();
+        Coordinates coordinates = null;
+        int cepBase = 13060471;
+        int[] offsets = {0, 1, -1, 2, -2, 3, -3};
+
+        for (int offset : offsets) {
+            int cep = cepBase + offset;
+
+            try {
+                String json = restTemplate.getForObject(
+                        "https://brasilapi.com.br/api/cep/v2/" + cep,
+                        String.class
+                );
+
+                if (json != null && json.contains("latitude")) {
+                    String latitude = json.split("\"latitude\":\"")[1].split("\"")[0].trim();
+                    String longitude = json.split("\"longitude\":\"")[1].split("\"")[0].trim();
+                    coordinates = new Coordinates(Double.valueOf(latitude), Double.valueOf(longitude));
+                    break;
+                }
+
+            } catch (Exception ignored) {}
+        }
+        assertNotNull(coordinates, "It should give an approximated location");
+        System.out.println(coordinates);
+    }
+
 }
