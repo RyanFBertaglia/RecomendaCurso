d7cd68d82134284a7dbe05b6862ae6a62cf0ac8f
Ryan Fernandes Bertaglia <ryanfernandesbertaglia@gmail.com>
2026-03-15 16:01:15 -0300

    feat auth service

---
+++ src/main/java/com/recommend/server/service/AuthService.java
@@ -0,0 +1,96 @@
+package com.recommend.server.service;
+
+import com.recommend.server.dto.AuthResponse;
+import com.recommend.server.dto.LoginRequest;
+import com.recommend.server.dto.RegisterRequest;
+import com.recommend.server.dto.UserDTO;
+import com.recommend.server.model.User;
+import com.recommend.server.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Objects;
+
+@Service
+public class AuthService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    JwtService jwtService;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    public AuthResponse register(RegisterRequest request) {
+
+        if (userRepository.existsByEmail(request.email()))
+            throw new RuntimeException("Email já existe");
+
+        User user = new User();
+
+        user.setName(request.name());
+        user.setEmail(request.email());
+        user.setPassword(passwordEncoder.encode(request.password()));
+
+        userRepository.save(user);
+
+        String token = jwtService.generateToken(user);
+
+        return new AuthResponse(token);
+    }
+
+    public AuthResponse login(LoginRequest request) {
+        User user = userRepository.findByEmail(request.email())
+                .orElseThrow();
+
+        if (!passwordEncoder.matches(request.password(), user.getPassword())) {
+            throw new RuntimeException("Senha inválida");
+        }
+
+        String token = jwtService.generateToken(user);
+        return new AuthResponse(token);
+    }
+
+    public AuthResponse refresh(HttpServletRequest request) {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer "))
+            throw new RuntimeException("Token ausente");
+
+        String token = authHeader.substring(7);
+        String email = jwtService.extractUserEmail(token);
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow();
+
+        if (!jwtService.isTokenValid(token, user))
+            throw new RuntimeException("Token inválido");
+
+        String newToken = jwtService.generateToken(user);
+
+        return new AuthResponse(newToken);
+    }
+
+    public UserDTO getUser() {
+        String email = (String) Objects.requireNonNull(SecurityContextHolder
+                        .getContext()
+                        .getAuthentication())
+                .getPrincipal();
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow();
+
+        return user.getUserDTO();
+    }
+}
---
+++ src/test/java/com/recommend/server/auth/AuthServiceTest.java
@@ -0,0 +1,97 @@
+package com.recommend.server.auth;
+
+import com.recommend.server.dto.AuthResponse;
+import com.recommend.server.dto.LoginRequest;
+import com.recommend.server.dto.RegisterRequest;
+import com.recommend.server.model.User;
+import com.recommend.server.repository.UserRepository;
+import com.recommend.server.service.AuthService;
+import com.recommend.server.service.JwtService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthServiceTest {
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    PasswordEncoder passwordEncoder;
+
+    @Mock
+    JwtService jwtService;
+
+    @Mock
+    AuthenticationManager authenticationManager;
+
+    @InjectMocks
+    AuthService authService;
+
+    @Test
+    void shouldRegisterUser() {
+
+        RegisterRequest request =
+                new RegisterRequest("Ryan","ryan@email.com","123456");
+
+        when(userRepository.existsByEmail("ryan@email.com"))
+                .thenReturn(false);
+        when(passwordEncoder.encode("123456"))
+                .thenReturn("hashed");
+
+        authService.register(request);
+        verify(userRepository).save(any(User.class));
+    }
+
+    @Test
+    void shouldLoginAndReturnToken() {
+        LoginRequest request =
+                new LoginRequest("ryan@email.com","123456");
+
+        User user = new User();
+        user.setEmail("ryan@email.com");
+
+        when(userRepository.findByEmail("ryan@email.com"))
+                .thenReturn(Optional.of(user));
+        when(jwtService.generateToken(user))
+                .thenReturn("jwt-token");
+
+        AuthResponse response = authService.login(request);
+        assertEquals("jwt-token", response.token());
+    }
+
+    @Test
+    void shouldRefreshToken() {
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+
+        when(request.getHeader("Authorization"))
+                .thenReturn("Bearer old-token");
+
+        User user = new User();
+        user.setEmail("ryan@email.com");
+
+        when(jwtService.extractUserEmail("old-token"))
+                .thenReturn("ryan@email.com");
+        when(userRepository.findByEmail("ryan@email.com"))
+                .thenReturn(Optional.of(user));
+        when(jwtService.isTokenValid("old-token", user))
+                .thenReturn(true);
+        when(jwtService.generateToken(user))
+                .thenReturn("new-token");
+
+        AuthResponse response = authService.refresh(request);
+        assertEquals("new-token", response.token());
+    }
+}
