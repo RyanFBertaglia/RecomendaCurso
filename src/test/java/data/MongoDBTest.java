ecf7ac664d3a10979943431cd89314585576f7f8
Ryan Fernandes Bertaglia <ryanfernandesbertaglia@gmail.com>
2026-05-05 21:28:56 -0300

    test connection with mongodb

---
+++ src/test/java/data/MongoDBTest.java
@@ -0,0 +1,48 @@
+package data;
+
+import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import org.bson.Document;
+
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(value = "classpath:application.properties")
+public class MongoDBTest {
+
+    @Value(value = "${mongodb.uri}")
+    String uri;
+
+    @Test
+    void contextLoads() {
+        System.out.println(uri);
+    }
+
+    @Test
+    void testMongoDB() {
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(uri))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
