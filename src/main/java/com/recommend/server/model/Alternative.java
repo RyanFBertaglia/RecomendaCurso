dfad34abcd6136f932824fe42f3813541191267a
Ryan Fernandes Bertaglia <ryanfernandesbertaglia@gmail.com>
2026-05-15 19:15:48 -0300

    build fafyl question model

--- src/main/java/com/recommend/server/model/Alternative.java
+++ src/main/java/com/recommend/server/model/Alternative.java
@@ -1,4 +1,13 @@
 package com.recommend.server.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Alternative {
+    private Integer id;
+    private String text;
+    private Character dimension;
+    private Double weight;
 }
--- src/main/java/com/recommend/server/model/Question.java
+++ src/main/java/com/recommend/server/model/Question.java
@@ -1,4 +1,21 @@
 package com.recommend.server.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
 public class Question {
+    private Integer id;
+    private String text;
+    private List<Alternative> alternatives;
+
+    public Alternative getAlternative(Integer id) {
+        return alternatives.stream()
+                .filter(alternative -> alternative.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
 }
